9f90df8e4fe89b10149288cf5ca5277c181c4f32
NERDYLIZARD <bouasavanhhop@gmail.com>
2018-04-20 21:12:26 +0530

    MongoDB validation error handler

--- routes/api/index.js
+++ routes/api/index.js
@@ -2,8 +2,50 @@
  * Created on 20-Apr-18.
  */
 const router = require('express').Router();
+const MongoError = require('mongoose').Error;
 
 // API Routes
 router.use('/cards', require('./cards'));
+
+router.use(function (err, req, res, next) {
+
+//// Ideal Validation Error Format
+  // {
+  //   code:
+  //   message:
+  //   errors: [
+  //     {
+  //       code:
+  //       message:
+  //       resource:
+  //       field:
+  //     }
+  //   ]
+  // }
+
+  // handle MongoDB validation errors
+  if (err.name === 'ValidationError' && err instanceof MongoError) {
+    return res.status(422).json({
+      code: err.name,
+      message: err.message,
+      errors: Object.keys(err.errors).reduce((errors, key) => {
+
+        // create each error according to a key
+        const error = {
+          code: err.errors[key].type,
+          message: err.errors[key].message,
+          // resource: find out how to get the model that throws the error
+          field: key,
+        };
+        // push the error into an errors array
+        errors.push(error);
+
+        return errors;
+      }, [])
+    });
+  }
+
+  return next(err);
+});
 
 module.exports = router;
