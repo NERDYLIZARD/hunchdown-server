3dfc9924c0869e4b2731aec20435bb9e9110a459
NERDYLIZARD <bouasavanhhop@gmail.com>
2018-06-27 09:47:52 +0530

    create `requester`

--- test/api/integration/hunches/GET-hunches.js
+++ test/api/integration/hunches/GET-hunches.js
@@ -2,14 +2,11 @@
  * Created on 25-Jun-18.
  */
 require('../../../helpers/api-integration.helper');
-const mongoose = require('mongoose');
-const request = require('supertest');
-const app = require('../../../../app');
 const HunchDataFactory = require('../../../../utils/test/data-factories/HunchDataFactory');
+const requester = require('../../../helpers/api-integration/requester');
 
-const Hunch = mongoose.model('Hunch');
+const Hunch = require('../../../../models/hunch');
 const hunchDataFactory = new HunchDataFactory();
-const baseUrl = '/api/hunches';
 
 before(async () => {
   // seed a hunch
@@ -18,32 +15,36 @@ before(async () => {
 });
 
 describe('GET /hunches', () => {
+
+  const baseUrl = '/api/hunches';
+  const api = requester();
+
   context('Success', () => {
 
     it('returns status 200', async () => {
-      const response = await request(app).get(baseUrl);
+      const response = await api.get(baseUrl);
       expect(response.status).to.eql(200);
     });
 
     it('returns header with `Link`, `X-Current-Page` and `X-Total-Pages`', async () => {
-      const response = await request(app).get(baseUrl);
+      const response = await api.get(baseUrl);
 
-      expect(response.header.link).to.not.be.undefined;
+      expect(response.header.link).to.exist;
 
-      expect(response.header['x-current-page']).to.not.be.undefined;
+      expect(response.header['x-current-page']).to.exist;
       expect(response.header['x-current-page']).to.eql('1');
 
-      expect(response.header['x-total-pages']).to.not.be.undefined;
+      expect(response.header['x-total-pages']).to.exist;
     });
 
     it('returns JSON array', async () => {
-      const response = await request(app).get(baseUrl);
+      const response = await api.get(baseUrl);
       expect(response.body).to.be.an('array');
     });
 
     it('returns object with correct props', async () => {
       const expectedProps = hunchDataFactory.getObjectProps();
-      const response = await request(app).get(baseUrl);
+      const response = await api.get(baseUrl);
       const sampleKeys = Object.keys(response.body[0]);
 
       expectedProps.forEach(key => expect(sampleKeys).to.include(key));
---
+++ test/helpers/api-integration/requester.js
@@ -0,0 +1,57 @@
+/**
+ * Created on 27-Jun-18.
+ */
+const supertest = require('supertest');
+const app = require('../../../app');
+
+
+function requester () {
+  return {
+    get: _requestMaker('get'),
+    post: _requestMaker('post'),
+    put: _requestMaker('put'),
+    del: _requestMaker('del'),
+  };
+}
+
+function _requestMaker (method) {
+
+  return (route, send, query) => {
+    return new Promise((resolve, reject) => {
+
+      let request = supertest(app)[method](route)
+        .accept('application/json');
+
+      request
+        .query(query)
+        .send(send)
+        .end((err, response) => {
+          if (err) {
+            const parsedError = _parseError(err);
+            return reject(parsedError);
+          }
+          resolve(response);
+        });
+    });
+  };
+}
+
+
+/**
+ * parse error response to include http status so that we don't need to assert the status and body separately
+ */
+function _parseError (err) {
+
+  const parsedError = {
+    status: err.status,
+    code: err.body.code,
+    message: err.body.message,
+  };
+  if (err.body.errors) {
+    parsedError.errors = err.body.errors;
+  }
+
+  return parsedError;
+}
+
+module.exports = requester;
